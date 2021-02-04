698a559f45b558a0cb5902608386b0d5fc570617
Peter Tsiompanis <38920641+Tsiompanis@users.noreply.github.com>
2021-02-04 20:44:19 +0200

    Add files via upload

---
+++ bio/sketch.js
@@ -0,0 +1,49 @@
+let slider;
+let sel;
+let fyl;
+
+
+let img;
+function preload() {
+  img = loadImage('assets/Untitled-1.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  slider = createSlider(0, 100, 16, 1);
+  slider.position(100, 250);
+  slider.style('width', '500px');
+  sel = createSelect();
+  sel.position(10, 10);
+  sel.option('Αρσενικό');
+  sel.option('Θυληκό');
+  sel.changed(mySelectEvent);
+  
+}
+
+function draw() {
+  background(40);
+  let val = slider.value();
+  fill(255);
+  rect(40, 40, windowWidth-90, windowHeight-80, 20);
+
+  image(img, 0, 0, 100, 100);
+
+  fill(0);
+  textAlign(CENTER);
+  
+  textSize(30);
+  text("ΗΛΙΚΙΑ: " + val,20,220,400,400);
+}
+
+function mySelectEvent() {
+  // let item = sel.value();
+  // if(item === "Αρσενικό")
+  // {
+  // 	fyl = 1;
+  // }
+  // else()
+  // {
+  // 	fyl = 2;
+  // }
+}
